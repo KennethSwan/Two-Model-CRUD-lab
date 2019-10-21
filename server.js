da9ad614cf0fc6485614b9c8ea0c0737d8276deb
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-21 17:20:22 -0500

    created index page for national

--- server.js
+++ server.js
@@ -5,16 +5,15 @@ const methodOverride = require('method-override');
 require('./db/db')
 
 
+app.use(methodOverride('_method')); 
+app.use(bodyParser.urlencoded({extended: false}));
 
+const nationalController = require('./cotrollers/national.js')
+app.use('/national', nationalController);
 
-
-
-
-
-
-
-
-
+app.get('/', (req, res) => {
+	res.render('index.ejs')
+})
 
 
 app.listen(3000, () => {
