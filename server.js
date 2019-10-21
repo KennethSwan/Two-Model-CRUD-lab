d1fe041170235ffb7ac7e2e11e1089c385c088e8
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-21 16:46:50 -0500

    server is listening

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express'); 
+const app = express(); 
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override'); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+	console.log('server listening on port', 3000);
+})
